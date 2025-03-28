bc0a69259b47fa75428700e1822b5463ef46221c
vicky <vickyempire723@gmail.com>
2025-03-28 15:46:46 +0530

    correct the npm build errors

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -10,7 +10,7 @@ import blogData from "@/data/singleBlogData.json";
 
 export default function BlogDetails() {
   const { slug } = useParams();
-  const [allBlogs, setAllBlogs] = useState([]);
+  const [setAllBlogs] = useState([]);
   const [blog, setBlog] = useState(null);
   const [isApiBlog, setIsApiBlog] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
