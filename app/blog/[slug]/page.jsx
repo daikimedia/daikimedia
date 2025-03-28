ec5a385016e3cffbcce386be47852695e9abd775
priyaclove <priya.cloveode@gmail.com>
2025-03-28 20:08:24 +0530

    correct the blog code

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -117,6 +117,10 @@ export default function BlogDetails() {
           content={decodeHtmlEntities(blog.description || blog.content?.substring(0, 150) || "Blog post details")}
         />
       </Head>
+      <div style={{ display: "none" }}>
+  {allBlogs.length > 0 && <p>Blogs fetched successfully</p>}
+</div>
+
       <PrimaryNavbar />
       <main className="flex flex-col items-center justify-center min-h-screen">
         <PageHero subtitle="BLOG Details" title="Recent blogs created <br/> by Daikai Media" />
