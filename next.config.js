be140f060776663281bec416537d80e738482875
vicky <vickyempire723@gmail.com>
2025-03-31 14:01:23 +0530

    compressing the size of html file

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/blogs/:slug',
-        destination: 'https://cms.daikimedia.com/api/blogs/:slug',
+        destination: 'https://cms.daikimedia.com/api/blogs/:slug ',
       },
     ];
   },
