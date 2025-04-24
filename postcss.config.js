c8563c1b87e60d1069f5115af4c0c678b704d283
priyaclove <priya.cloveode@gmail.com>
2025-04-24 16:10:04 +0530

    correct error

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+  plugins: [
+    require('tailwindcss'),
+    
+    require('autoprefixer'),
+    
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./pages/**/*.js', './components/**/*.js'], 
+      safelist: [], // Add any CSS classes you want to preserve here
+    }),
+    
+    require('cssnano')({ preset: 'default' }),
+  ],
+};
