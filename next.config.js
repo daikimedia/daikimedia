258ba4f9c3b7d4c5df65b60051d190626d96659d
daikimedia <63772859+daikimedia@users.noreply.github.com>
2025-04-24 13:42:43 +0530

    Revert "reduce the dom size"

--- next.config.js
+++ next.config.js
@@ -3,25 +3,6 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true', // Only analyze when explicitly set
 });
-const Critters = require('critters-webpack-plugin');
-
-module.exports = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.plugins.push(new Critters({
-        preload: 'swap',
-        compress: true,
-      }));
-    }
-    return config;
-  },
-};
-// next.config.js
-module.exports = {
-  experimental: {
-    optimizeCss: true,
-  },
-};
 
 const nextConfig = {
   async rewrites() {
