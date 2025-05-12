76f5aa3a306d960390de6a8d56e1b9a67c0b02e0
priyaclove <priya.cloveode@gmail.com>
2025-05-12 15:49:13 +0530

    correct canonical tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -44,7 +44,7 @@ export default function RootLayout({ children }) {
         />
         <link rel="preload" as="image" href="https://www.daikimedia.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftestimg.81f0ff33.avif&w=1920&q=75" type="image/avif" />
 
-        <link rel="canonical" href="https://www.daikimedia.com/" />	
+        {/* <link rel="canonical" href="https://www.daikimedia.com/" />	 */}
         {/* Open Graph Meta Tags */}
         <meta property="og:title" content="Daiki Media" />
         <meta property="og:description" content="Daiki Media provides expert SEO and website development services." />
