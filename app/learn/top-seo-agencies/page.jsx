bef7812095913c2a65cc69f6edfec3724851b2da
manishcloveode <manish.cloveode@gmail.com>
2025-06-09 18:17:54 +0530

    Add filter feature

--- app/learn/top-seo-agencies/page.jsx
+++ app/learn/top-seo-agencies/page.jsx
@@ -1,4 +1,4 @@
-import SeoFirmSection from "../../components/seo-firms/seo-firms-section"
+import SeoFirmSection from "../../../components/top-seo-firms/seo-firms-section"
 import PrimaryNavbar from "@/components/navbar/PrimaryNavbar";
 import Footer from "@/components/footer/Footer";
 export default function SeoFirm() {
